5ad6304ae9bab4c3b7d10b8bc97be534439402c2
namvoyager <namttp@voyager-hcm.com>
2023-05-29 16:57:04 +0700

    MAC-26: Delete multiple roles

--- app/Repositories/Contracts/RoleRepository.php
+++ app/Repositories/Contracts/RoleRepository.php
@@ -2,7 +2,12 @@
 
 namespace App\Repositories\Contracts;
 
+use App\Models\Role;
+
 interface RoleRepository extends Repository
 {
-    //
+    /**
+     * Handle delete multiple roles at the same time.
+     */
+    public function deleteMultiple(array $roleIds, ?Role $auth = null): ?bool;
 }
