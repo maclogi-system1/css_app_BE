7ccbc38f32bba126eabbfa40f2aedbcdb97ce37f
Namttp-Voyager <namttp@voyager-hcm.com>
2023-09-28 17:25:51 +0700

    MAC-217: Update sheet default

--- database/migrations/2023_09_27_102013_add_is_default_to_mq_sheets_table.php
+++ database/migrations/2023_09_27_102013_add_is_default_to_mq_sheets_table.php
@@ -16,14 +16,28 @@ return new class extends Migration {
         Schema::table('mq_sheets', function (Blueprint $table) {
             $table->boolean('is_default')->after('name')->default(0);
         });
-        $groupMqSheets = DB::table('mq_sheets')->get()->groupBy('store_id');
 
-        foreach ($groupMqSheets as $mqSheets) {
-            DB::table('mq_sheets')->where('id', $mqSheets->first()->id)->update([
-                'name' => MqSheet::DEFAULT_NAME,
-                'is_default' => 1,
-            ]);
+        try {
+            DB::beginTransaction();
+            // Updates existing sheets with the default name.
+            $sheetDefault = DB::table('mq_sheets')->where('name', MqSheet::DEFAULT_NAME);
+            $ignoredStore = $sheetDefault->pluck('store_id');
+            $sheetDefault->update(['is_default' => 1]);
+
+            // Update the default sheet for each store.
+            $groupMqSheets = DB::table('mq_sheets')->whereNotIn('store_id', $ignoredStore)->get()->groupBy('store_id');
+            foreach ($groupMqSheets as $mqSheets) {
+                DB::table('mq_sheets')->where('id', $mqSheets->first()->id)->update([
+                    'name' => MqSheet::DEFAULT_NAME,
+                    'is_default' => 1,
+                ]);
+            }
+
+            DB::commit();
+        } catch (Throwable) {
+            DB::rollBack();
         }
+
     }
 
     /**
