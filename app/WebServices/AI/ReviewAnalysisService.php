b0444ab3d4107bed32385b0925279f4fd0027b1c
Nguyen Thanh Nhan <nhannt92@voyager-hcm.com>
2023-10-11 11:46:02 +0700

    MAC-235: Handle query year-month for sales amount per user
    MAC-235: Handle query year-month review analysis

--- app/WebServices/AI/ReviewAnalysisService.php
+++ app/WebServices/AI/ReviewAnalysisService.php
@@ -7,6 +7,7 @@ use App\Support\Traits\HasMqDateTimeHandler;
 use App\WebServices\Service;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
 
 class ReviewAnalysisService extends Service
 {
@@ -15,8 +16,12 @@ class ReviewAnalysisService extends Service
     /**
      * Get review analysis summary by store_id.
      */
-    public function getReviewSummary($storeId, array $filters = []): Collection
+    public function getReviewSummary($storeId, array $filters = [], bool $isMonthQuery = false): Collection
     {
+        if ($isMonthQuery) {
+            return $this->getYearMonthReviewSummary($storeId, $filters);
+        }
+
         $dateRangeFilter = $this->getDateRangeFilter($filters);
         $fromDate = $dateRangeFilter['from_date']->format('Y-m-d');
         $toDate = $dateRangeFilter['to_date']->format('Y-m-d');
@@ -73,8 +78,12 @@ class ReviewAnalysisService extends Service
     /**
      * Get data chart reviews trend.
      */
-    public function getChartReviewsTrends($storeId, array $filters = []): Collection
+    public function getChartReviewsTrends($storeId, array $filters = [], bool $isMonthQuery = false): Collection
     {
+        if ($isMonthQuery) {
+            return $this->getChartYearMonthReviewsTrends($storeId, $filters);
+        }
+
         $dateRangeFilter = $this->getDateRangeFilter($filters);
         $fromDate = $dateRangeFilter['from_date']->format('Y-m-d');
         $toDate = $dateRangeFilter['to_date']->format('Y-m-d');
@@ -88,42 +97,115 @@ class ReviewAnalysisService extends Service
             ->selectRaw('date, SUM(review_all) as total_review, SUM(item_all.all_purchaser_all) as total_purchase')
             ->groupBy('store_id', 'date')
             ->get();
-        $result = ! is_null($result) ? $result->toArray() : [];
-
-        $dateTimeRange = $this->getDateTimeRange(
-            $dateRangeFilter['from_date'],
-            $dateRangeFilter['to_date'],
-            [
-                'format' => 'Y/m/d',
-                'step' => '1 day',
-            ]
-        );
+        $result = ! is_null($result) ? $result->groupBy('date')->toArray() : [];
 
         $data = collect();
 
-        if (count($result) > 0) {
-            foreach ($dateTimeRange as $date) {
-                $dateStr = str_replace('/', '', $date);
-                $reviewDataItem = collect($result)->filter(function ($item) use ($dateStr) {
-                    return Arr::get($item, 'date') == $dateStr;
-                })->first();
+        foreach ($result as $date => $reviewDataItem) {
+            $totalReview = intval(Arr::get($reviewDataItem[0] ?? [], 'total_review', 0));
+            $totalPurchase = intval(Arr::get($reviewDataItem[0] ?? [], 'total_purchase', 0));
+            $data->add([
+                'date' => substr($date, 0, 4).'/'.substr($date, 4, 2).'/'.substr($date, 6, 2),
+                'review_all' => $totalReview,
+                'review_writing_rate' => $totalPurchase > 0 ? round($totalReview / $totalPurchase, 2) : 0,
+            ]);
+        }
 
-                if (! is_null($reviewDataItem)) {
-                    $totalReview = intval(Arr::get($reviewDataItem, 'total_review', 0));
-                    $totalPurchase = intval(Arr::get($reviewDataItem, 'total_purchase', 0));
-                    $data->add([
-                        'date' => $date,
-                        'review_all' => $totalReview,
-                        'review_writing_rate' => $totalPurchase > 0 ? round($totalReview / $totalPurchase, 2) : 0,
-                    ]);
-                } else {
-                    $data->add([
-                        'date' => $date,
-                        'review_all' => 0,
-                        'review_writing_rate' => 0,
-                    ]);
-                }
-            }
+        return collect([
+            'success' => true,
+            'status' => 200,
+            'data' => $data,
+        ]);
+    }
+
+    /**
+     * Get review analysis summary by store_id.
+     */
+    private function getYearMonthReviewSummary($storeId, array $filters = []): Collection
+    {
+        $dateRangeFilter = $this->getDateRangeFilter($filters);
+        $fromDate = $dateRangeFilter['from_date']->format('Y-m');
+        $toDate = $dateRangeFilter['to_date']->format('Y-m');
+        $fromDateStr = str_replace('-', '', date('Ym', strtotime($fromDate)));
+        $toDateStr = str_replace('-', '', date('Ym', strtotime($toDate)));
+
+        $lastMonthStart = $dateRangeFilter['from_date']->subMonth()->format('Y-m');
+        $lastMonthEnd = $dateRangeFilter['to_date']->subMonth()->format('Y-m');
+        $lastMonthStartStr = str_replace('-', '', date('Ym', strtotime($lastMonthStart)));
+        $lastMonthEndStr = str_replace('-', '', date('Ym', strtotime($lastMonthEnd)));
+
+        $totalReview = ItemsData::where('store_id', $storeId)
+            ->whereRaw('
+            (SUBSTRING(date, 1, 6) >= ? AND SUBSTRING(date, 1, 6) <= ? ) 
+            OR (SUBSTRING(date, 1, 6) >= ? AND SUBSTRING(date, 1, 6) <= ? )
+            ', [$fromDateStr, $toDateStr, $lastMonthStartStr, $lastMonthEndStr])
+            ->selectRaw('
+                    SUM(CASE WHEN SUBSTRING(date, 1, 6) >= ? AND SUBSTRING(date, 1, 6) <= ? THEN review_all ELSE 0 END) as current_month_count,
+                    SUM(CASE WHEN SUBSTRING(date, 1, 6) >= ? AND SUBSTRING(date, 1, 6) <= ? THEN review_all ELSE 0 END) as previous_month_count
+            ', [$fromDateStr, $toDateStr, $lastMonthStartStr, $lastMonthEndStr])
+            ->groupBy('store_id')
+            ->first();
+        $totalReview = ! is_null($totalReview) ? $totalReview->toArray() : [];
+        $currentMonthReview = intval(Arr::get($totalReview, 'current_month_count', 0));
+        $previousMonthReview = intval(Arr::get($totalReview, 'previous_month_count', 0));
+
+        $totalPurchase = ItemsData::where('store_id', $storeId)
+            ->where(DB::raw('SUBSTRING(date, 1, 6)'), '>=', $fromDateStr)
+            ->where(DB::raw('SUBSTRING(date, 1, 6)'), '<=', $toDateStr)
+            ->join('items_data_all as item_all', 'item_all.items_data_all_id', '=', 'items_data.items_data_all_id')
+            ->selectRaw('SUM(item_all.all_purchaser_all) as total_purchase')
+            ->groupBy('store_id')
+            ->first();
+        $totalPurchase = ! is_null($totalPurchase) ? $totalPurchase->toArray() : [];
+        $totalPurchaseNum = Arr::get($totalPurchase, 'total_purchase', 0);
+
+        $data = collect();
+        $data->add([
+            'store_id' => $storeId,
+            'from_date' => Arr::get($filters, 'from_date'),
+            'to_date' => Arr::get($filters, 'to_date'),
+            'cumulative_review_all' => $currentMonthReview,
+            'review_writing_rate' => $totalPurchaseNum > 0 ? round($currentMonthReview / $totalPurchaseNum, 2) : 0,
+            'new_review_all' => ($currentMonthReview - $previousMonthReview) > 0 ? ($currentMonthReview - $previousMonthReview) : 0,
+        ]);
+
+        return collect([
+            'success' => true,
+            'status' => 200,
+            'data' => $data,
+        ]);
+    }
+
+    /**
+     * Get data chart reviews trend by year-month.
+     */
+    public function getChartYearMonthReviewsTrends($storeId, array $filters = []): Collection
+    {
+        $dateRangeFilter = $this->getDateRangeFilter($filters);
+        $fromDate = $dateRangeFilter['from_date']->format('Y-m');
+        $toDate = $dateRangeFilter['to_date']->format('Y-m');
+        $fromDateStr = str_replace('-', '', date('Ym', strtotime($fromDate)));
+        $toDateStr = str_replace('-', '', date('Ym', strtotime($toDate)));
+
+        $result = ItemsData::where('store_id', $storeId)
+            ->where(DB::raw('SUBSTRING(date, 1, 6)'), '>=', $fromDateStr)
+            ->where(DB::raw('SUBSTRING(date, 1, 6)'), '<=', $toDateStr)
+            ->join('items_data_all as item_all', 'item_all.items_data_all_id', '=', 'items_data.items_data_all_id')
+            ->selectRaw('SUBSTRING(date, 1, 6) as date, SUM(review_all) as total_review, SUM(item_all.all_purchaser_all) as total_purchase')
+            ->groupBy('store_id', DB::raw('SUBSTRING(date, 1, 6)'))
+            ->get();
+        $result = ! is_null($result) ? $result->groupBy('date')->toArray() : [];
+
+        $data = collect();
+
+        foreach ($result as $date => $reviewDataItem) {
+            $totalReview = intval(Arr::get($reviewDataItem[0] ?? [], 'total_review', 0));
+            $totalPurchase = intval(Arr::get($reviewDataItem[0] ?? [], 'total_purchase', 0));
+            $data->add([
+                'date' => substr($date, 0, 4).'/'.substr($date, 4, 2),
+                'review_all' => $totalReview,
+                'review_writing_rate' => $totalPurchase > 0 ? round($totalReview / $totalPurchase, 2) : 0,
+            ]);
         }
 
         return collect([
--- app/WebServices/AI/SalesAmntPerUserService.php
+++ app/WebServices/AI/SalesAmntPerUserService.php
@@ -3,6 +3,7 @@
 namespace App\WebServices\AI;
 
 use App\Models\KpiRealData\ShopAnalyticsDaily;
+use App\Models\KpiRealData\ShopAnalyticsMonthly;
 use App\Support\Traits\HasMqDateTimeHandler;
 use App\WebServices\Service;
 use Illuminate\Support\Arr;
@@ -14,8 +15,12 @@ class SalesAmntPerUserService extends Service
     /**
      * Get data ads analysis summary from AI.
      */
-    public function getChartSummarySalesAmntPerUser($storeId, array $filters)
+    public function getChartSummarySalesAmntPerUser($storeId, array $filters, bool $isMonthQuery = false)
     {
+        if ($isMonthQuery) {
+            return $this->getChartYearMonthSummarySalesAmntPerUser($storeId, $filters);
+        }
+
         $dateRangeFilter = $this->getDateRangeFilter($filters);
         $fromDate = $dateRangeFilter['from_date']->format('Y-m-d');
         $toDate = $dateRangeFilter['to_date']->format('Y-m-d');
@@ -63,8 +68,12 @@ class SalesAmntPerUserService extends Service
     /**
      * Get data compare sale amount per user with last year data from AI.
      */
-    public function getSalesAmntPerUserComparisonTable($storeId, array $filters)
+    public function getSalesAmntPerUserComparisonTable($storeId, array $filters, bool $isMonthQuery = false)
     {
+        if ($isMonthQuery) {
+            return $this->getYearMonthSalesAmntPerUserComparisonTable($storeId, $filters);
+        }
+
         $dateRangeFilter = $this->getDateRangeFilter($filters);
         $fromDate = $dateRangeFilter['from_date']->format('Y-m-d');
         $toDate = $dateRangeFilter['to_date']->format('Y-m-d');
@@ -133,6 +142,106 @@ class SalesAmntPerUserService extends Service
                 'to_date' => Arr::get($filters, 'to_date'),
                 'chart_pv' => $dataFake,
             ]),
+        ]);
+    }
+
+    /**
+     * Get data ads analysis summary from AI.
+     */
+    private function getChartYearMonthSummarySalesAmntPerUser($storeId, array $filters)
+    {
+        $dateRangeFilter = $this->getDateRangeFilter($filters);
+        $fromDate = $dateRangeFilter['from_date']->format('Y-m');
+        $toDate = $dateRangeFilter['to_date']->format('Y-m');
+        $fromDateStr = str_replace('-', '', date('Ym', strtotime($fromDate)));
+        $toDateStr = str_replace('-', '', date('Ym', strtotime($toDate)));
+
+        $dailyResult = ShopAnalyticsMonthly::where('date', '>=', $fromDateStr)
+            ->where('date', '<=', $toDateStr)
+            ->join(
+                'shop_analytics_monthly_sales_amnt_per_user as monthly_sales',
+                'monthly_sales.sales_amnt_per_user_id',
+                '=',
+                'shop_analytics_monthly.sales_amnt_per_user_id'
+            )
+            ->selectRaw('store_id, date, AVG(monthly_sales.all_value) as sales_amnt_per_user')
+            ->groupBy('store_id', 'date')
+            ->orderBy('date')
+            ->get()
+            ->groupBy('date');
+        $dailyResult = ! is_null($dailyResult) ? $dailyResult->toArray() : [];
+
+        $data = collect();
+        foreach ($dailyResult as $dailyKey => $dailyItem) {
+            $listStoreValues = collect();
+            foreach ($dailyItem as $storeVal) {
+                $listStoreValues->add([
+                    'display_name' => Arr::get($storeVal, 'store_id'),
+                    'store_id' => Arr::get($storeVal, 'store_id'),
+                    'sales_amnt_per_user' => floatval(Arr::get($storeVal, 'sales_amnt_per_user', 0)),
+                ]);
+            }
+            $data->add([
+                'date' => substr($dailyKey, 0, 4).'/'.substr($dailyKey, 4, 2),
+                'stores_sales_amnt_per_user' => $listStoreValues,
+            ]);
+        }
+
+        return collect([
+            'success' => true,
+            'status' => 200,
+            'data' => $data,
+        ]);
+    }
+
+    /**
+     * Get data compare sale amount per user with last year data from AI by year-month.
+     */
+    private function getYearMonthSalesAmntPerUserComparisonTable($storeId, array $filters)
+    {
+        $dateRangeFilter = $this->getDateRangeFilter($filters);
+        $fromDate = $dateRangeFilter['from_date']->format('Y-m');
+        $toDate = $dateRangeFilter['to_date']->format('Y-m');
+        $fromDateStr = str_replace('-', '', date('Ym', strtotime($fromDate)));
+        $toDateStr = str_replace('-', '', date('Ym', strtotime($toDate)));
+
+        $dailyResult = ShopAnalyticsMonthly::where('date', '>=', $fromDateStr)
+            ->where('date', '<=', $toDateStr)
+            ->join(
+                'shop_analytics_monthly_sales_amnt_per_user as monthly_sales',
+                'monthly_sales.sales_amnt_per_user_id',
+                '=',
+                'shop_analytics_monthly.sales_amnt_per_user_id'
+            )
+            ->selectRaw('
+                date, 
+                AVG(monthly_sales.all_value) as all_value, 
+                AVG(monthly_sales.pc) as pc,
+                AVG(monthly_sales.app) as app,
+                AVG(monthly_sales.device) as device
+            ')
+            ->orderBy('date')
+            ->groupBy('date')
+            ->get()
+            ->groupBy('date');
+        $dailyResult = ! is_null($dailyResult) ? $dailyResult->toArray() : [];
+
+        $data = collect();
+        foreach ($dailyResult as $dailyKey => $dailyItem) {
+            $salesData = $dailyItem[0];
+            $data->add([
+                'date' => substr($dailyKey, 0, 4).'/'.substr($dailyKey, 4, 2),
+                'total' => floatval(Arr::get($salesData, 'all_value', 0)),
+                'pc' => floatval(Arr::get($salesData, 'pc', 0)),
+                'app' => floatval(Arr::get($salesData, 'app', 0)),
+                'phone' =>  floatval(Arr::get($salesData, 'device', 0)),
+            ]);
+        }
+
+        return collect([
+            'success' => true,
+            'status' => 200,
+            'data' => $data,
         ]);
     }
 }
