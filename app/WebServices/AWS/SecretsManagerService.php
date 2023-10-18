ae765d83266a866668acefec925417d0eefa2231
Namttp-Voyager <namttp@voyager-hcm.com>
2023-10-18 10:31:47 +0700

    MAC-275: Config set password connection real data

--- app/WebServices/AWS/SecretsManagerService.php
+++ app/WebServices/AWS/SecretsManagerService.php
@@ -9,9 +9,26 @@ class SecretsManagerService
 {
     public static function getPassword(?string $secretName = null)
     {
+        if (in_array(env('APP_ENV'), ['dev', 'local', 'development'])) {
+            return env('AI_DB_PASSWORD', '');
+        }
+
         $secretArray = static::getSecretValue($secretName);
 
         return Arr::get($secretArray, 'password');
+    }
+
+    public static function getPasswordCache(?string $secretName = null)
+    {
+        if (cache()->has('aws_secret_password')) {
+            return cache('aws_secret_password');
+        }
+
+        $password = static::getPassword($secretName);
+
+        cache(['aws_secret_password' => $password], now()->addDays(7));
+
+        return $password;
     }
 
     public static function getSecretValue(?string $secretName = null): array
