962c163586ac90356eb6252245b0d873ab3a4e8a
Namttp <namttp@voyager-hcm.com>
2023-11-09 17:33:24 +0700

    MAC-331: Update response chart

--- app/Repositories/Eloquents/ValueChainRepository.php
+++ app/Repositories/Eloquents/ValueChainRepository.php
@@ -529,73 +529,82 @@ class ValueChainRepository extends Repository implements ValueChainRepositoryCon
 
         return $this->getListByStore($storeId, $filters)->map(fn ($valueChain) => [
             'store_id' => $valueChain->store_id,
-            'date' => $valueChain->date,
-
-            'number_of_categories_point' => $valueChain->number_of_categories_point,
-            'number_of_items_point' => $valueChain->number_of_items_point,
-            'product_utilization_rate_point' => $valueChain->product_utilization_rate_point,
-            'product_cost_rate_point' => $valueChain->product_cost_rate_point,
-            'low_product_reviews_point' => $valueChain->low_product_reviews_point,
-            'few_sold_out_items_point' => $valueChain->few_sold_out_items_point,
-
-            'purchase_form_point' => $valueChain->purchase_form_point,
-            'stock_value_point' => $valueChain->stock_value_point,
-            'product_utilization_rate_point' => $valueChain->product_utilization_rate_point,
-
-            'top_page_point' => $valueChain->top_page_point,
-            'category_page_point' => $valueChain->category_page_point,
-            'header_point' => $valueChain->header_point,
-            'product_page_point' => $valueChain->product_page_point,
-            'product_page_conversion_rate' => $valueChain->product_page_conversion_rate_point,
-            'product_thumbnail_point' => $valueChain->product_thumbnail_point,
-            'access_number_point' => $valueChain->access_number_point,
-            'featured_products_point' => $valueChain->featured_products_point,
-            'left_navigation_point' => $valueChain->left_navigation_point,
-            'header_large_banner_small_banner_point' => $valueChain->header_large_banner_small_banner_point,
-
-            'event_sales_ratio_point' => $valueChain->event_sales_ratio_point,
-            'sales_ratio_day_endings_0_5' => $valueChain->sales_ratio_day_endings_0_5_point,
-            'implementation_of_measures_point' => $valueChain->implementation_of_measures_point,
-            'coupon_effect_point' => $valueChain->coupon_effect_point,
-
-            'rpp_ad_point' => $valueChain->rpp_ad_point,
-            'rpp_ad_operation' => $valueChain->rpp_ad_operation_point,
-            'coupon_advance_point' => $valueChain->coupon_advance_point,
-            'rgroup_ad_point' => $valueChain->rgroup_ad_point,
-            'tda_ad_point' => $valueChain->tda_ad_point,
-            'sns_ad_point' => $valueChain->sns_ad_point,
-            'google_access_point' => $valueChain->google_access_point,
-            'instagram_access_point' => $valueChain->instagram_access_point,
-
-            'compatible_point' => $valueChain->compatible_point,
-            'shipping_fee_point' => $valueChain->shipping_fee_point,
-            'shipping_ratio_point' => $valueChain->shipping_ratio_point,
-            'mail_service_point' => $valueChain->mail_service_point,
-            'bundling_ratio_point' => $valueChain->bundling_ratio_point,
-            'gift_available_point' => $valueChain->gift_available_point,
-            'delivery_on_specified_day_point' => $valueChain->delivery_on_specified_day_point,
-            'delivery_preparation_period_point' => $valueChain->delivery_preparation_period_point,
-            'shipping_on_the_specified_date_point' => $valueChain->shipping_on_the_specified_date_point,
-            'shipping_according_to_the_delivery_date_point' => $valueChain->shipping_according_to_the_delivery_date_point,
-
-            'system_introduction' => $valueChain->system_introduction_point,
-            'order_through_rate_point' => $valueChain->order_through_rate_point,
-            'number_of_people_in_charge_of_ordering_point' => $valueChain->number_of_people_in_charge_of_ordering_point,
-
-            'thank_you_email_point' => $valueChain->thank_you_email_point,
-            'what_s_included_point' => $valueChain->what_s_included_point,
-            'follow_email_point' => $valueChain->follow_email_point,
-            'order_email_point' => $valueChain->order_email_point,
-            'shipping_email_point' => $valueChain->shipping_email_point,
-            'few_user_complaints_point' => $valueChain->few_user_complaints_point,
-
-            'email_newsletter_point' => $valueChain->email_newsletter_point,
-            're_sales_num_rate_point' => $valueChain->re_sales_num_rate_point,
-            'review_writing_rate_point' => $valueChain->review_writing_rate_point,
-            'review_measures_point' => $valueChain->review_measures_point,
-            'line_official' => $valueChain->line_official_point,
-            'instagram_followers_point' => $valueChain->instagram_followers_point,
-            'ltv_point' => $valueChain->ltv_point,
+            'date' => Carbon::create($valueChain->date)->format('Y/m'),
+            'merchandise' => round((
+                $valueChain->number_of_categories_point
+                + $valueChain->number_of_items_point
+                + $valueChain->product_utilization_rate_point
+                + $valueChain->product_cost_rate_point
+                + $valueChain->low_product_reviews_point
+                + $valueChain->few_sold_out_items_point
+            ) / 6, 2),
+            'purchase' => round((
+                $valueChain->purchase_form_point
+                + $valueChain->stock_value_point
+                + $valueChain->product_utilization_rate_point
+            ) / 3, 2),
+            'construction_production' => round((
+                $valueChain->top_page_point
+                + $valueChain->category_page_point
+                + $valueChain->header_point
+                + $valueChain->product_page_point
+                + $valueChain->product_page_conversion_rate_point
+                + $valueChain->product_thumbnail_point
+                + $valueChain->access_number_point
+                + $valueChain->featured_products_point
+                + $valueChain->left_navigation_point
+                + $valueChain->header_large_banner_small_banner_point
+            ) / 10, 2),
+            'event_sale' => round((
+                $valueChain->event_sales_ratio_point
+                + $valueChain->sales_ratio_day_endings_0_5_point
+                + $valueChain->implementation_of_measures_point
+                + $valueChain->coupon_effect_point
+            ) / 4, 2),
+            'advertisement' => round((
+                $valueChain->rpp_ad_point
+                + $valueChain->rpp_ad_operation_point
+                + $valueChain->coupon_advance_point
+                + $valueChain->rgroup_ad_point
+                + $valueChain->tda_ad_point
+                + $valueChain->sns_ad_point
+                + $valueChain->google_access_point
+                + $valueChain->instagram_access_point
+            ) / 8, 2),
+            'logistics' => round((
+                $valueChain->compatible_point
+                + $valueChain->shipping_fee_point
+                + $valueChain->shipping_ratio_point
+                + $valueChain->mail_service_point
+                + $valueChain->bundling_ratio_point
+                + $valueChain->gift_available_point
+                + $valueChain->delivery_on_specified_day_point
+                + $valueChain->delivery_preparation_period_point
+                + $valueChain->shipping_on_the_specified_date_point
+                + $valueChain->shipping_according_to_the_delivery_date_point
+            ) / 10, 2),
+            'orders' => round((
+                $valueChain->system_introduction_point
+                + $valueChain->order_through_rate_point
+                + $valueChain->number_of_people_in_charge_of_ordering_point
+            ) / 3, 2),
+            'customer_service' => round((
+                $valueChain->thank_you_email_point
+                + $valueChain->what_s_included_point
+                + $valueChain->follow_email_point
+                + $valueChain->order_email_point
+                + $valueChain->shipping_email_point
+                + $valueChain->few_user_complaints_point
+            ) / 6, 2),
+            'crm' => round((
+                $valueChain->email_newsletter_point
+                + $valueChain->re_sales_num_rate_point
+                + $valueChain->review_writing_rate_point
+                + $valueChain->review_measures_point
+                + $valueChain->line_official_point
+                + $valueChain->instagram_followers_point
+                + $valueChain->ltv_point
+            ) / 7, 2),
         ]);
     }
 }
