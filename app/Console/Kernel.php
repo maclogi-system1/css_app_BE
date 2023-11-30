bc7c770ae2c253fc70f5ceb6a9e18306d3794562
Namttp <namttp@voyager-hcm.com>
2023-11-30 15:59:57 +0700

    MAC-366: Update AI's url api

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -6,6 +6,7 @@ use App\Console\Commands\CreateDefaultValueChainsForShops;
 use App\Console\Commands\CreateMqSheetDefault;
 use App\Console\Commands\CreateStandardDeviation;
 use App\Console\Commands\ExecuteScheduledMacros;
+use App\Console\Commands\GetInferenceStorePred36m;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -20,6 +21,8 @@ class Kernel extends ConsoleKernel
         $schedule->command(CreateMqSheetDefault::class)->daily();
         $schedule->command(CreateDefaultValueChainsForShops::class)->monthlyOn(3, '5:0');
         $schedule->command(CreateStandardDeviation::class)->monthlyOn(1, '5:0');
+        $schedule->command(GetInferenceStorePred36m::class)->monthlyOn(1);
+        $schedule->command(GetInferenceStorePred36m::class, ['--generate-data'])->monthlyOn(3);
     }
 
     /**
